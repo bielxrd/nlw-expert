02bb499434d38ec5e60195794ff82318d9e36a18
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2024-02-07 17:18:26 -0300

    Finalizacao config tailwind

--- nlw-expert-notes/tailwind.config.js
+++ nlw-expert-notes/tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html',
-    './src/**/*.tsx',
-  ],
+  content: ["./index.html", "./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
