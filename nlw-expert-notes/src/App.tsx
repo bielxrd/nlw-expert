43892e79e3968d1ac758c33e342fd6b5f991d184
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2024-02-16 02:24:04 -0300

    feat: recebe dados da data da nota e do conteudo via props

--- nlw-expert-notes/src/App.tsx
+++ nlw-expert-notes/src/App.tsx
@@ -1,4 +1,6 @@
 import logo from './assets/Logo-nlw-expert.svg'
+import { NewNoteCard } from './components/new-note-card'
+import { NoteCard } from './components/node-card'
 
 export function App() {
 
@@ -19,49 +21,12 @@ export function App() {
       <div className='h-px bg-slate-700' />
 
       <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
-        <div className='rounded-md bg-slate-700 p-5 space-y-3'>
-          <span className='text-sm font-medium text-slate-200'>
-            Adicionar nota
-          </span>
+        <NewNoteCard />
+        <NoteCard date={new Date("2024-02-16")} content='Hello World' />
+        <NoteCard date={new Date(2023, 10, 22)} content='Hello World' />
+        <NoteCard date={new Date(2022, 0o3, 19)} content='Hello World' />
+        <NoteCard date={new Date("2024-02-16")} content='Hello World' />
 
-          <p className='text-sm leading-6 text-slate-400'>Grave uma nota em áudio que será convertida para texto automaticamente.</p>
-
-        </div>
-
-        <div className='rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative pointer-events-none'>
-          <span className='text-sm font-medium text-slate-300'>
-            há 2 dias
-          </span>
-
-          <p className='text-sm leading-6 text-slate-400'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque qui eum reprehenderit! Porro veniam voluptatibus assumenda reiciendis saepe omnis perspiciatis quod voluptate? Hic consectetur eos amet debitis nisi eius vel.
-          </p>
-
-          <div className='absolute botton-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0' />
-        </div>
-
-
-        <div className='rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative pointer-events-none'>
-          <span className='text-sm font-medium text-slate-300'>
-            há 4 dias
-          </span>
-
-          <p className='text-sm leading-6 text-slate-400'>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam autem, ex unde rem repudiandae vel fugiat qui illo consectetur sequi, quisquam facilis quod. Quos blanditiis labore sequi, totam aliquam illo?
-          </p>
-
-          <div className='absolute botton-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0' />
-        </div>
-
-        <div className='rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative pointer-events-none'>
-          <span className='text-sm font-medium text-slate-200'>
-            há 6 dias
-          </span>
-
-          <p className='text-sm leading-6 text-slate-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae cum optio modi, corrupti quo fugit voluptas tempore accusantium nam veritatis aspernatur, ullam beatae cupiditate maiores ad ea architecto labore alias?</p>
-
-          <div className='absolute botton-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0' />
-        </div>
 
       </div>
     </div >
--- nlw-expert-notes/src/components/node-card.tsx
+++ nlw-expert-notes/src/components/node-card.tsx
@@ -1,17 +1,21 @@
-export function NoteCard() {
+interface NoteCardProps {
+    date: Date;
+    content: string
+}
+
+export function NoteCard(props: NoteCardProps) {
     return (
 
-        <button className='rounded-md text-left bg-slate-800 p-5 space-y-3 outline-none overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus:ring-2 focus:ring-lime-400'>
+        <button className="rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus:ring-lime-400 outline-none">
             <span className='text-sm font-medium text-slate-300'>
-                há 2 dias
+                {props.date.toISOString()}
             </span>
 
             <p className='text-sm leading-6 text-slate-400'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque qui eum reprehenderit! Porro veniam voluptatibus assumenda reiciendis saepe omnis perspiciatis quod voluptate? Hic consectetur eos amet debitis nisi eius vel.
+                {props.content}
             </p>
 
-            <div className='absolute botton-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none' />
-
+            <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
         </button>
     )
 }
